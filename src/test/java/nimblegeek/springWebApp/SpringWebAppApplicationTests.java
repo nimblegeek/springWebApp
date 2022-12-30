6abedb5481d963201b436105db120375c655c0c8
Jonas Achouri Sihlén <jonasasihlengmail.com>
2022-12-30 16:45:48 +0100

    Initial commit.

---
+++ src/test/java/nimblegeek/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nimblegeek.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
