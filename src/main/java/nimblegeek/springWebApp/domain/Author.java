b70ea66aecb1c82889615a767c0d9fd247faefe2
Jonas Achouri Sihlén <jonasasihlengmail.com>
2022-12-31 08:39:09 +0100

    Setting up JPA entities and preparing table connections.

--- src/main/java/nimblegeek/springWebApp/domain/Author.java
+++ src/main/java/nimblegeek/springWebApp/domain/Author.java
@@ -1,10 +1,20 @@
 package nimblegeek.springWebApp.domain;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
--- src/main/java/nimblegeek/springWebApp/domain/Book.java
+++ src/main/java/nimblegeek/springWebApp/domain/Book.java
@@ -1,11 +1,22 @@
 package nimblegeek.springWebApp.domain;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -15,6 +26,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -44,7 +63,6 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-
 
 
     }
